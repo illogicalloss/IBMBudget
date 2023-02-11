107fcc150518a62a62786fa0e818bf9325824ee2
Sam <illogicalloss@gmail.com>
2023-02-11 01:16:22 -0500

    Finish the app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,34 @@
+import React, { useContext} from 'react';
+import { AppContext } from '../context/AppContext';
+const Budget = () => {
+    const { budget, dispatch, expenses, currency } = useContext(AppContext);
 
+    const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+        if (val<totalExpenses){
+            alert("Can't do reduce budget below allocated values.")
+        }else{
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: val, 
+            })
+        }
+    }
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}
+            <input
+                        required='required'
+                        type='number'
+                        step="10"
+                        id='cost'
+                        value={budget}
+                        onInput={(event) => changeBudget(event.target.value)}>
+                        </input>
+            </span>
+        </div>
+    );
+};
+export default Budget;
